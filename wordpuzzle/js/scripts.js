9b0f9e6069831039668da9e275afa42abb5c4202
Mary Warrington and Will Murphy <wilmurph@gmail.com>
2016-01-21 13:33:49 -0800

    complete functioning js and html for word puzzle

--- wordpuzzle/js/scripts.js
+++ wordpuzzle/js/scripts.js
@@ -3,8 +3,17 @@ var vowelConverter = function(wordInput) {
   return convertedWord;
 }
 
+$(document).ready(function() {
+  $("#formInput").submit(function(event) {
+    var wordInput = $("input#userInput").val();
+    var wordResults = vowelConverter(wordInput);
 
 
-// if (wordInput.match(/[aeiou]/i); {
-//   .replace(/aeiou/i, "-");
-// }
+    // return wordResults
+    $(".results").show();
+    $("#formInput").hide();
+    $("p#showResults").text(wordResults);
+    // $("#userInput").text("");
+    event.preventDefault();
+  });
+});

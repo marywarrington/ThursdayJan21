f974a6f49eae09a5f305d70e8a299d7383485bbb
Mary Warrington and Will Murphy <marywarrington@gmail.com>
2016-01-21 10:31:41 -0800

    Re-do all js to complete project in a simpler way

--- count/js/scripts.js
+++ count/js/scripts.js
@@ -1,3 +1,17 @@
+var countBy = function(addNumber, maxNumber) {
+  var answer = "";
+  for (var i = 0; i <= maxNumber; i += addNumber) {
+    answer = answer + i + ",";
+  }
+    answer = answer.slice(2, answer.length-1);
+  return answer;
+}
+
+
+
+
+
+
 // var countBy = function(convertNumber) {
 //   for (index = 0, index = convertNumber, index ++) {
 //     return
@@ -5,16 +19,16 @@
 // [index = 0, index = convertNumber, index++]
 //
 // };
-var makeArray = function(elementIn) {
-  var firstInputArray = [];
-  firstInputArray.push(convertNumber(elementIn))
-  return firstInputArray;
-};
-// return firstInputArray;
-
-var convertNumber = function(firstInput) {
-  return parseInt(firstInput);
-};
+// var makeArray = function(elementIn) {
+//   var firstInputArray = [];
+//   firstInputArray.push(convertNumber(elementIn))
+//   return firstInputArray;
+// };
+// // return firstInputArray;
+//
+// var convertNumber = function(firstInput) {
+//   return parseInt(firstInput);
+// };
 
 
 
--- count/specs/specs.js
+++ count/specs/specs.js
@@ -1,17 +1,33 @@
+describe('countBy', function() {
+  it("it will count by 1 to maxNumber", function () {
+    expect(countBy(1, 10)).to.equal("1,2,3,4,5,6,7,8,9,10");
+  });
+  it("will county by 2 to maxNumber", function () {
+    expect(countBy(2, 10)).to.equal("2,4,6,8,10");
+  });
+  it("will count by addNumber to maxNumber", function () {
+    expect(countBy(3, 30)).to.equal("3,6,9,12,15,18,21,24,27,30");
+  });
+});
+
+
+
+
+
 // describe('countBy', function() {
 //   it("will return an array counting up to firstInput", function() {
 //     expect(countBy(5)).to.eql([1,2,3,4,5]);
 //   });
 // });
 
-describe('makeArray', function() {
-  it("will take the number and put it in an array", function () {
-    expect(makeArray(30)).to.eql([ 30 ]);
-  });
-});
-
-describe('convertNumber', function() {
-  it("will convert a string into a number", function() {
-    expect(convertNumber("30")).to.equal(30);
-  });
-});
+// describe('makeArray', function() {
+//   it("will take the number and put it in an array", function () {
+//     expect(makeArray(30)).to.eql([ 30 ]);
+//   });
+// });
+//
+// describe('convertNumber', function() {
+//   it("will convert a string into a number", function() {
+//     expect(convertNumber("30")).to.equal(30);
+//   });
+// });

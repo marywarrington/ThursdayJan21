b1933b92507b1ff98b47198a907f8c161632c431
Nicholas Jensen <Hreacon@users.noreply.github.com>
2016-01-21 16:07:20 -0800

    Add code review comments
    White space looks good! many less wasted lines. Keep improving!

--- count/js/scripts.js
+++ count/js/scripts.js
@@ -1,12 +1,15 @@
 var countBy = function(addNumber, maxNumber) {
   var answer = "";
+  // i, while obvious, is not very descriptive. If you start i at addNumber you wont have to cut off 0
   for (var i = 0; i <= maxNumber; i += addNumber) {
+    // WHY U NO SPACE?
     answer = answer + i + ",";
   }
+  // this could be refactored out
     answer = answer.slice(2, answer.length-1);
   return answer;
 }
-
+// If you used an array instead of a string for answer then used join to turn the array into a string you can control how the sentence looks better
 
 
 
